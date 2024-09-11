d7bd929fd44abf9d4944995f1ff2c0d2c6f22e46
Hafiz Mudassir Husain <160108451+HafizMudassirHusain@users.noreply.github.com>
2024-09-11 12:00:53 -0700

    Update app.js

--- Firebase/emailAndPassword/app.js
+++ Firebase/emailAndPassword/app.js
@@ -3,13 +3,14 @@ import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.12.3/fireba
 import { getAuth,createUserWithEmailAndPassword ,signInWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/10.12.3/firebase-auth.js'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDN3qVhonsM16PTUhuj4GnLUtMmdLdOdh4",
-    authDomain: "firstpro-6970d.firebaseapp.com",
-    projectId: "firstpro-6970d",
-    storageBucket: "firstpro-6970d.appspot.com",
-    messagingSenderId: "543837521380",
-    appId: "1:543837521380:web:6fc8f9cbfd4c0c5ff5b3f1"
-  };
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT_ID.appspot.com",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID",
+  measurementId: "YOUR_MEASUREMENT_ID"
+};
   const app = initializeApp(firebaseConfig);
  const auth = getAuth(app);
 var getbtn = document.getElementById('subtn')
