b5b05737780a459f503e9c7d48f019877e1c7869
Hafiz Mudassir Husain <160108451+HafizMudassirHusain@users.noreply.github.com>
2024-09-11 11:57:59 -0700

    Update app.js

--- Firebase/E-Commerse/main/app.js
+++ Firebase/E-Commerse/main/app.js
@@ -3,14 +3,15 @@
   import { getAuth, createUserWithEmailAndPassword,
     signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js";
 
-  const firebaseConfig = {
-    apiKey: "AIzaSyA4vh55jxmsSyFocm0iVirOKWKIi913giM",
-    authDomain: "e-commerce-bee09.firebaseapp.com",
-    projectId: "e-commerce-bee09",
-    storageBucket: "e-commerce-bee09.appspot.com",
-    messagingSenderId: "1089328260784",
-    appId: "1:1089328260784:web:c1025fe271b00ca8c63d75"
-  };
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT_ID.appspot.com",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID",
+  measurementId: "YOUR_MEASUREMENT_ID"
+};
   const app = initializeApp(firebaseConfig);
   const auth = getAuth(app);
 
